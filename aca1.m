38e5f5494cc3784b87bc0118184ae3103df0a927
saripahmaulani <89384444+saripahmaulani@users.noreply.github.com>
2021-08-23 18:15:40 +0800

    Add files via upload

---
+++ aca1.m
@@ -0,0 +1,22 @@
+12/3.5,(3+5/4)^2
+(0.25^2+0.75^2)^1/2,2/(6/0.3)
+A=25,B=50,C=125,D=89;
+X=A+B+C, Y=A/(B+B)
+Z=D^ (A/B)+C
+luas=1
+ke1_1=1
+time=1
+time=1
+time_from_strat=1
+%valid yang tidak muncul
+x=pi/6;y=0.001;
+sqrt(y),exp(-x),sin(x)
+cos(2*x),tan(3*x),log10(y)
+log2(y),log(y)
+p=9+16*i;q=-9+16*i;
+r=p*q,s=p/q,p-r
+r+s,p^2,sqrt(q)
+abs(p),angle(p)
+abs(q),angle(q)
+abs(r),angle(r)
+abs(s),angle(s)
